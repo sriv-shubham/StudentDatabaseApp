ca4b13ccfadbb6930edda31f9b2db59f782fe1c5
sriv-shubham <shubham.136@gmail.com>
2018-11-17 16:56:11 +0530

    outline

--- DatabaseApp/src/studentdatabaseapp/Student.java
+++ DatabaseApp/src/studentdatabaseapp/Student.java
@@ -1,17 +1,29 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
-	private String year;
+	private int year;
 	private String courses;
 	private int studentId;
 	private int tutionBalance;
 	private int costOfCourse;
 	
 	//Constructor: prompt user to enter name and year
-	
+	public Student() {
+		System.out.println("Enter first Name");
+		Scanner in = new Scanner(System.in);
+		this.firstName=in.nextLine();
+		System.out.println("Enter last Name");
+		Scanner in2 = new Scanner(System.in);
+		this.lastName=in2.nextLine();
+		System.out.println("Enter Year:\n1 - Fresher\n2 - Second\n3 - Third\n4 - Final\n");
+		Scanner in3=new Scanner(System.in);
+		this.year=in3.nextInt();
+	}
 	//Generate an ID
 	
 	//Enroll in courses
