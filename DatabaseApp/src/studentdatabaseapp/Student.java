9b4e3b8f7e97720736c377ab2fd2f1b62cab51cb
sriv-shubham <shubham.136@gmail.com>
2018-11-18 12:57:21 +0530

    final

--- DatabaseApp/src/studentdatabaseapp/Student.java
+++ DatabaseApp/src/studentdatabaseapp/Student.java
@@ -8,10 +8,10 @@ public class Student {
 	private String lastName;
 	private int year;
 	private String courses;
-	private int studentId;
-	private int tutionBalance;
-	private int costOfCourse;
-	
+	private String studentId;
+	private int tutionBalance=0;
+	private static int costOfCourse=600;
+	private static int id=1000;
 	//Constructor: prompt user to enter name and year
 	public Student() {
 		System.out.println("Enter first Name");
@@ -23,14 +23,40 @@ public class Student {
 		System.out.println("Enter Year:\n1 - Fresher\n2 - Second\n3 - Third\n4 - Final\n");
 		Scanner in3=new Scanner(System.in);
 		this.year=in3.nextInt();
+		setid();
+		System.out.println(firstName+" "+lastName+" "+year+" "+studentId);
 	}
 	//Generate an ID
-	
+	private void setid() {
+		id++;
+		this.studentId= year+""+id;
+	}
 	//Enroll in courses
-	
+	public void enroll() {
+		while(true) {
+			System.out.println("Enter Course (Q to quit)");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if(!course.equals("Q")) {
+				courses=courses+"\n"+course;
+				tutionBalance=tutionBalance+costOfCourse;
+			}
+			else break;
+		}
+		System.out.println(courses);
+	}
 	//View Balance
-	
+	public void viewbalance() {
+		System.out.println("Your Balance is : "+tutionBalance);
+	}
 	//Pay Tution
-	
+	public void pay() {
+		viewbalance();
+		System.out.println("Enter amount you want to pay");
+		Scanner in = new Scanner(System.in);
+		tutionBalance-=in.nextInt();
+		System.out.println("Thank for Payment");
+		viewbalance();
+	}
 	//Show Status
 }
--- DatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
+++ DatabaseApp/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,9 +4,10 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		// Ask how many users you want to add
-		
+		Student stu1 = new Student();
 		// create n numbers of new student
-
+		stu1.enroll();
+		stu1.pay();
 	}
 
 }
